395102098ed0297fc63728cc5f77f9a933ecaa22
bjcochrane <cochrabj@miamioh.edu>
2014-05-23 09:51:53 -0400

    Added HW function

--- R/zZOO422.R
+++ R/zZOO422.R
@@ -89,18 +89,6 @@ pr <-round(pr,2)
 out <-cat(paste("chi-square = ",chi), paste("probability(<.05) = ",pr), paste("deg. freedom = ",df),sep="\n")
 out
 }
-hw <- function(obs){
-n <-sum(obs)
-  p <- round((2*obs[1]+obs[2])/(2*n),4)
-q <-1-p
-print(paste("p=",p,"q=",q),sep=" ",digits=3)
-exp <-as.integer(round(c(p^2*n,2*p*q*n,q^2*n),0))
-print(cbind(obs,exp))
-
-chi <-sum((obs-exp)^2/exp)
-pr <-1-pchisq(chi,1)
-print(paste("chi squared =",round(chi,3),"p = ",round(pr,3), "with 1 d. f.",sep=" "))
-}
 
 # The following uses input from read.HapMap.data in chopsticks and returns a file formatted as saving for csv input into Genalex
 hap2gen <- function(x){
