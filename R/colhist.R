1616b9ac1f470329c6a1bb6197f7f5fee923b4fd
bjcochrane <cochrabj@miamioh.edu>
2015-01-08 14:19:15 -0500

    Fixed hist to include na.rm=TRUE

--- R/colhist.R
+++ R/colhist.R
@@ -7,11 +7,11 @@ colhist =function(dat,breaks=20,tail=2,labs =c("X","Number","Title"),xr=c(0,0)){
   
   if(identical(xr,c(0,0))) xr=(range(flhist$breaks))
   if(tail==2){
-    q <-quantile(dat,c(.025,.975))    
+    q <-quantile(dat,c(.025,.975),na.rm=TRUE)    
     hist(dat,nclass=length(flhist$mids),col=ifelse(flhist$mids<q[1],"red",ifelse(flhist$mids>q[2],"red","white")),xlab=labs[1],ylab=labs[2],main=labs[3],xlim=xr)
   }
   if(tail==1){
-    q <-quantile(dat,.95)
+    q <-quantile(dat,.95,na.rm=TRUE)
     hist(dat,nclass=length(flhist$mids),col=ifelse(flhist$mids>q,"red","white"),xlab=labs[1],ylab=labs[2],main=labs[3],xlim=xr)
   }
   #  print(q)
