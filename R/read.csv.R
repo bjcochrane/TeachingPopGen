0c31448d599a478330cd4188de0b8957614a40b5
bjcochrane <cochrabj@miamioh.edu>
2015-01-15 11:06:59 -0500

    function and doc to read https data

--- R/read.csv.R
+++ R/read.csv.R
@@ -1,6 +1,8 @@
-#Function to read https url
+#Function to read https url-default conditions are those of read.csv
 
-read.https <-function(url){
+read.https <-function(url,header = TRUE, sep = ",", quote = "\"", dec = ".", 
+fill = TRUE, comment.char = "", ...){
   csv <-getURL(url)
-  read.csv(textConnection(csv))
+  read.table(textConnection(csv),header = header, sep = sep, quote = quote, 
+             dec = dec, fill = fill, comment.char = comment.char, ...)
 }
