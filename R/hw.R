9c07f3ed37ffa759b77639fcec0464b1c062281c
bjcochrane <cochrabj@miamioh.edu>
2015-02-03 10:12:52 -0500

    Corrected chi-squared; added F calculation

--- R/hw.R
+++ R/hw.R
@@ -4,17 +4,19 @@
 # Returns a list with observed, expected, chi-squared, and probability
 hw <- function(obs,print=TRUE){
   n <-sum(obs)
-  p <- round((2*obs[1]+obs[2])/(2*n),4)
+  p <- (2*obs[1]+obs[2])/(2*n)
   q <-1-p
-  exp <-as.integer(round(c(p^2*n,2*p*q*n,q^2*n),0))
+  exp <-(c(p^2*n,2*p*q*n,q^2*n))
   chi <-sum((obs-exp)^2/exp)
   pr <-1-pchisq(chi,1)
+  f <-1-obs[2]/exp[2]
   if(print){
   print(paste("p=",p,"q=",q,sep=" "),digits=3,quote=FALSE)
-  print(cbind(obs,exp))
+  print(cbind(obs,exp=round(exp,0)))
   print(paste("chi squared =",round(chi,3),"p = ",round(pr,3), "with 1 d. f.",sep=" "),quote=FALSE)
+  print(paste("F = ",round(f,4),sep=" "),quote=FALSE)
   }
-  out <-list(obs,exp,c(p,q),chi,pr)
-  names(out) <-c("Observed","Expected","Allele_freqs","Chisq","prob")
+  out <-list(obs,exp,c(p,q),chi,pr,f)
+  names(out) <-c("Observed","Expected","Allele_freqs","Chisq","prob","F")
   out
 }
