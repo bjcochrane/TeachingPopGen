92e4823e1a78fa9f4174a2efbefed3aa012f8ee4
bjcochrane <cochrabj@miamioh.edu>
2015-02-15 14:50:03 -0500

    Changed to allow variable p

--- R/driftPlot.R
+++ R/driftPlot.R
@@ -1,8 +1,8 @@
-driftPlot <-function(N, nreps=10){
+driftPlot <-function(N, nreps=10,p=0.5){
   ngen=length(N) # Set length to run
   plot(c(0,ngen),c(0,1),type="n", xlab="Generation", ylab="p") # create a blank plot
   sapply(c(1:nreps),function (x) {
-    pi <-rep(.5,ngen)
+    pi <-rep(p,ngen)
     gen <-c(1:ngen)
     for (i in 1:(ngen-1)){
       pi[i+1] <-rbinom(1,2*N[i],pi[i])/(2*N[i])
