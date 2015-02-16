3bd6e347ac51c10c64a0dd0a8fc963a5630006ec
bjcochrane <cochrabj@miamioh.edu>
2015-02-16 11:01:56 -0500

    Print suppression option added

--- R/driftPlot.R
+++ R/driftPlot.R
@@ -1,13 +1,13 @@
-driftPlot <-function(N, nreps=10,p=0.5){
+driftPlot <-function(N, nreps=10,p=0.5,print=TRUE){
   ngen=length(N) # Set length to run
-  plot(c(0,ngen),c(0,1),type="n", xlab="Generation", ylab="p") # create a blank plot
+  if(print) plot(c(0,ngen),c(0,1),type="n", xlab="Generation", ylab="p") # create a blank plot
   sapply(c(1:nreps),function (x) {
     pi <-rep(p,ngen)
     gen <-c(1:ngen)
     for (i in 1:(ngen-1)){
       pi[i+1] <-rbinom(1,2*N[i],pi[i])/(2*N[i])
     }
-    lines(gen,pi)
+    if(print) lines(gen,pi)
     pi
   }
   )
