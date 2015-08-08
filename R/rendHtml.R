b9fc56eb2df0417d29f7c4b3cfdd944dc856207b
bjcochrane <cochrabj@miamioh.edu>
2015-08-08 09:37:06 -0400

    Modified rendHTML to add date to output file name.

--- R/rendHtml.R
+++ R/rendHtml.R
@@ -1,5 +1,6 @@
 # converts .Rmd file to .html and stores in a folder called html
 rendHtml <-function(file,out.dir="html"){
-  f.name <-paste(file,".Rmd",sep="")
-  rmarkdown::render(f.name,output_dir=paste('./',out.dir,sep=""))
+  f.name <-paste(file,"Rmd",sep=".")
+  outfile <-paste(file,format(Sys.Date(), "%d%y"),"html",sep=".")
+  rmarkdown::render(f.name,output_file=outfile,output_dir=paste('./',out.dir,sep=""))
 }
