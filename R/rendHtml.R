08a37abbfadbe394f62dbfffb21ec9ad8f96ed12
bjcochrane <cochrabj@miamioh.edu>
2015-08-09 08:29:24 -0400

    Revert rendHTML back to original without date.

--- R/rendHtml.R
+++ R/rendHtml.R
@@ -1,6 +1,6 @@
 # converts .Rmd file to .html and stores in a folder called html
 rendHtml <-function(file,out.dir="html"){
   f.name <-paste(file,"Rmd",sep=".")
-  outfile <-paste(file,format(Sys.Date(), "%d%y"),"html",sep=".")
+  outfile <-paste(file,"html",sep=".")
   rmarkdown::render(f.name,output_file=outfile,output_dir=paste('./',out.dir,sep=""))
 }
