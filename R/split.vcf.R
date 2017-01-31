e72a8f2a2434716605849703bee3fb2269abb18c
bjcochrane <cochrabj@miamioh.edu>
2017-01-31 14:29:20 -0500

    Another tweak

--- R/split.vcf.R
+++ R/split.vcf.R
@@ -4,7 +4,7 @@ split.vcf <-function(dat.vcf,pops){
   data(poplist,package="TeachingPopGen")
   test <-split(poplist.1000g,poplist.1000g$Population.code)
   test2 <-test[which(names(test) %in% pops)]
-  nms2 <-sapply(test2, function (x) x$Sample.name)
+  nms2 <-sapply(test2, function (x) as.character(x$Sample.name))
   dat.spl <-lapply(nms2, function(x) dat.vcf[x,])
   dat.spl
 }
